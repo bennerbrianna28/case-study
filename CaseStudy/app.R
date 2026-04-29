84009c8b0db12e00d37d6c7a41e47e8935d70301
unknown <you@example.com>
2026-04-29 13:37:54 -0700

    Push the working, updated app

--- CaseStudy/app.R
+++ CaseStudy/app.R
@@ -1,50 +1,101 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
+# libraries
+library(vroom)
+library(tidyverse)
 library(shiny)
+
+# load in the data
+if (!exists("injuries")) {
+  injuries <- vroom::vroom("injuries.tsv.gz")
+  products <- vroom::vroom("products.tsv")
+  population <- vroom::vroom("population.tsv")
+}
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  # give user choice between number of injuries or population standardised rate
+  fluidRow(
+    column(8,
+           selectInput("code", "Product",
+                       choices = setNames(products$prod_code, products$title),
+                       width = "100%"
+           )
+    ),
+    column(2, selectInput("y", "Y axis", c("rate", "count"))) # choose between rate and count
+  ),
+  fluidRow(
+    column(4, tableOutput("diagnosis")),
+    column(4, tableOutput("body_part")),
+    column(4, tableOutput("location"))
+  ),
+  fluidRow(
+    column(12, plotOutput("age_sex"))
+  ),
+  # interactively explore narratives
+  fluidRow(
+    column(2, actionButton("story", "Tell me a story")), # action button
+    column(10, textOutput("narrative")) # text output
+  )
 )
+
+
+# find the 5 most common injuries by count
+count_top <- function(df, var, n = 5) {
+  df %>%
+    mutate({{ var }} := fct_lump(fct_infreq({{ var }}), n = n)) %>%
+    group_by({{ var }}) %>%
+    summarise(n = as.integer(sum(weight)))
+}
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  selected <- reactive(injuries %>% filter(prod_code %in% input$code))
+  
+  output$diagnosis <- renderTable(count_top(selected(), diag), width = "100%",
+                                  caption = paste("Diagnosis"))
+  output$body_part <- renderTable(count_top(selected(), body_part), width = "100%",
+                                  caption = paste("Body part inured"))
+  output$location <- renderTable(count_top(selected(), location), width = "100%",
+                                 caption = paste("Where patient was when injury occured"))
+  
+  summary <- reactive({
+    selected() %>%
+      count(age, sex, wt = weight) %>%
+      left_join(population, by = c("age", "sex")) %>%
+      mutate(rate = n / population * 1e4)
+  })
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  output$age_sex <- renderPlot({
+    if (input$y == "count") {
+      summary() %>%
+        ggplot(aes(age, n, colour = sex)) +
+        geom_line() +
+        labs(y = "Estimated number of injuries") +
+        scale_x_continuous(expand = c(0, 0)) +
+        scale_y_continuous(expand = c(0, 5)) +
+        coord_cartesian(xlim = c(0, 84)) +
+        theme_bw()
+    } else {
+      summary() %>%
+        ggplot(aes(age, rate, colour = sex)) +
+        geom_line(na.rm = TRUE) +
+        labs(y = "Injuries per 10,000 people") +
+        scale_x_continuous(expand = c(0, 0)) +
+        scale_y_continuous(expand = c(0, 5)) +
+        coord_cartesian(xlim = c(0, 84)) +
+        theme_bw()
+    }
+  }, res = 96)
+  
+  # create a reactive that only updates when action button is pushed 
+  # or underlying data changes
+  narrative_sample <- eventReactive(
+    list(input$story, selected()),
+    selected() %>% pull(narrative) %>% sample(1)
+  )
+  output$narrative <- renderText(narrative_sample())
 }
 
 # Run the application 
